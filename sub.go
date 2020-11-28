86f156e77c08ccfedf505b45a47ebd5d5f76091c
eaglexiang <eagle.xiang@outlook.com>
2020-11-28 14:23:30 +0800

    提交 sample

--- sub.go
+++ sub.go
@@ -6,12 +6,10 @@ import (
 	"reflect"
 )
 
-var (
-	ErrNotSubConfig = errors.New("sub item for key is not sub config")
-)
+var ErrNotSubConfig = errors.New("sub item for key is not sub config")
 
-func errNotSubConfig(sub interface{}) (err error) {
+func errNotSubConfig(sub interface{}, key string) (err error) {
 	t := reflect.TypeOf(sub)
-	err = fmt.Errorf("%w: %s", ErrNotSubConfig, t.String())
+	err = fmt.Errorf("%w: %s(%s)", ErrNotSubConfig, t.String(), key)
 	return
 }
